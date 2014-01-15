f358df727e315bf5e7b00740511cfe9be8a1ceb5
Oliver Lohoff <oliverlohoff@gmail.com>
2014-01-15 15:03:39 +0100

    Initial Commit

---
+++ config/autoload.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2014 Leo Feyer
+ *
+ * @package Tosrus
+ * @link    https://contao.org
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ */
+
+
+/**
+ * Register the templates
+ */
+TemplateLoader::addFiles(array
+(
+	'j_tosrus' => 'system/modules/tosrus/templates',
+));
